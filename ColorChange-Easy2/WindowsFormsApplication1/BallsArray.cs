4311bff5a5ec0a77936bf5dd284a461410bc5942
anastasijaC <kabranova.boka@hotmail.com>
2016-05-12 19:05:46 +0200

    dopolnuvanje na Easy

---
+++ ColorChange-Easy2/WindowsFormsApplication1/BallsArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+   public class BallsForChange
+    {
+        int X { set; get; }
+        int Y { set; get; }
+        public BallsForChange (int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Draw (Graphics g)
+        {
+            Pen pen1 = new Pen(Color.Red,3);
+            Pen pen2 = new Pen(Color.Yellow,3);
+            Pen pen3 = new Pen(Color.Green,3);
+            Pen pen4 = new Pen(Color.Blue,3);
+            g.DrawEllipse(pen1,X, Y, 14, 14);
+            g.DrawEllipse(pen2, X + 2, Y + 2, 10, 10);
+            g.DrawEllipse(pen3, X + 4, Y + 4, 6, 6);
+            g.DrawEllipse(pen4, X + 6, Y + 6, 2, 2);
+           
+        }
+
+    }
+}
