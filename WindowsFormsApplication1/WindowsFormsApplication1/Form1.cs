70e7f7822982759caf50864f6265067688b1acf1
BokaK <BokaK>
2016-05-12 18:00:04 +0200

    Izbor na nivo

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Scene scene { get; set; }
+        public int count { get; set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            scene = new Scene();
+            scene.AddShape(100, 100, 0);
+            count = 0;
+            Invalidate();
+            timer1.Start();
+            timer1.Interval=1000;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            scene.Draw(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            
+            
+            scene.AddShape(100, 100, count);
+            Invalidate();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Scene.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+     public class Scene
+    {
+        public List<Shape> Shapes;
+
+        public Scene()
+        {
+            Shapes = new List<Shape>();
+        }
+
+        public void AddShape(float x, float y, int c)
+        {
+
+            Shape s = null;
+           
+            s = new Square(x, y, c);
+           
+            Shapes.Add(s);
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach (Shape s in Shapes)
+            {
+                s.Draw(g);
+            }
+        }
+
+    }
+}
