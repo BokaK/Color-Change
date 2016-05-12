b1d5bd18fb907f3017f371e1383d5f7b478428dc
BokaK <BokaK>
2016-05-12 18:40:44 +0200

    pocetna implementacija na easy

---
+++ ColorChange-Easy1/WindowsFormsApplication1/Scene.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+     public class Scene
+    {
+        public List<Shape> Shapes;
+        public Ball ball { set; get; }
+
+        public Scene()
+        {
+            Shapes = new List<Shape>();
+            ball = new Ball();
+        }
+
+        public void AddShape(float x, float y, int c)
+        {
+
+            Shape s = null;
+           
+            s = new Square(x, y, c);
+           
+            Shapes.Add(s);
+
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach (Shape s in Shapes)
+            {
+                s.Draw(g);
+            }
+            ball.Draw(g);
+        }
+
+    }
+}
