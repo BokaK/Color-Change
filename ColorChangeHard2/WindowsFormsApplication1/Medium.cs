eade86aff7e815e4dae003697ed787adc4206988
simona069 <markova.simona@yahoo.com>
2016-05-15 00:28:26 +0200

    dodavanje novi figuri vo hard

---
+++ ColorChangeHard2/WindowsFormsApplication1/Medium.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Medium : Form
+    {
+        public int X { set; get; }
+        public Scene scene { get; set; }
+        public int count { get; set; }
+        public int brojac { get; set; }
+        public int clock { set; get; }
+        public Medium()
+        {
+            InitializeComponent();
+            X = 0;
+            clock = 40;
+            scene = new Scene(1);
+            scene.AddLinii(120, 560, 0);
+            scene.balls.Add(new BallsForChange(142, 110));
+            scene.balls.Add(new BallsForChange(141, 220));
+            scene.balls.Add(new BallsForChange(141, 340));
+            scene.balls.Add(new BallsForChange(141, 460));
+            count = 0;
+            brojac = 0;
+            scene.AddIks(120, 80, count);
+            scene.AddVertLinii(0, 200, count);
+            scene.AddLinii(120, 440, count);
+            scene.AddLinii(120, 560, count);
+            scene.AddRotateSquare(120, 320, brojac);
+            Invalidate();
+            timer1Medium.Start();
+            timer1Medium.Interval = 1000;
+           // timer1.Start();
+           // timer1.Interval = 500;
+        }
+
+        private void Medium_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Medium_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.DimGray);
+            scene.Draw(e.Graphics);
+        }
+
+        private void timer1Medium_Tick(object sender, EventArgs e)
+        {
+            count++;
+            scene.Shapes.Clear();
+            scene.AddIks(120, 80, count);
+            if (X > 250)
+            {
+                X = 0;
+            }
+            else
+            X += 20;
+            scene.AddVertLinii(X, 200, count);
+            scene.AddLinii(120, 440, count);
+            scene.AddLinii(120, 560, count);
+            Invalidate();
+            brojac++;
+            scene.AddRotateSquare(120, 320, brojac);
+            Clock();
+
+        }
+        private void Clock()
+        {
+
+            clock--;
+            label1.Text = String.Format("00:{0:00}", clock);
+            if (clock <= 10)
+            {
+                label1.ForeColor = Color.Red;
+            }
+            if (clock == 0)
+            {
+
+                MessageBox.Show("GAME OVER!");
+                timer1.Stop();
+                DialogResult = System.Windows.Forms.DialogResult.Cancel;
+
+            }
+        }
+
+
+        private void Medium_MouseClick(object sender, MouseEventArgs e)
+        {
+            scene.ball.Y -= 30;
+           // check(scene.ball.Y, scene.ball.count, count);
+        }
+        public void check(int y, int countBall, int c)
+        {
+            if (y < 570 && y > 540)
+            {
+                if ((c % 4 == 0 && countBall == 0) || (c % 4 == 2 && countBall == 1) )
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Game over!");
+                    this.Close();
+                }
+            }
+            else if (y < 500 && y > 470)
+            {
+                if ((c % 4 == 2 && countBall == 0) || (c % 4 == 0 && countBall == 1))
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Game over!");
+                    this.Close();
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ColorChangeHard2/WindowsFormsApplication1/Triangle.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Triangle :Shape
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+        public int count { set; get; }
+        public int flag { set; get; }
+        public Triangle (int x, int y, int c, int f)
+            :base(x,y)
+        {
+            X = x;
+            Y = y;
+            count = c;
+            flag = f;
+        }
+        public override void Draw(Graphics g)
+        {
+            Pen p1 = new Pen(Color.Firebrick,3);
+            Pen p2 = new Pen(Color.IndianRed,3);
+            Pen p3 = new Pen(Color.RosyBrown,3);
+            Pen p4 = new Pen(Color.MistyRose,3);
+            Pen p = new Pen(Color.Transparent);
+            if (flag == 0)
+            {
+                if (count % 4 == 0)
+                {
+                    g.DrawLine(p1, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p2, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p3, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 1)
+                {
+                    g.DrawLine(p3, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p1, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p2, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 2)
+                {
+                    g.DrawLine(p2, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p3, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p1, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 3)
+                {
+                    g.DrawLine(p, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p, X, Y, X + 64, Y);
+
+                }
+            }
+            if (flag == 1)
+            {
+                if (count % 4 == 0)
+                {
+                    g.DrawLine(p2, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p1, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p3, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 1)
+                {
+                    g.DrawLine(p3, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p2, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p1, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 2)
+                {
+                    g.DrawLine(p1, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p3, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p2, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 3)
+                {
+                    g.DrawLine(p, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p, X, Y, X + 64, Y);
+
+                }
+            }
+            if (flag == 2)
+            {
+                if (count % 4 == 0)
+                {
+                    g.DrawLine(p3, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p4, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p2, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 1)
+                {
+                    g.DrawLine(p2, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p3, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p4, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 2)
+                {
+                    g.DrawLine(p4, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p2, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p3, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 3)
+                {
+                    g.DrawLine(p, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p, X, Y, X + 64, Y);
+
+                }
+
+            }
+            if (flag == 3)
+            {
+                if (count % 4 == 0)
+                {
+                    g.DrawLine(p4, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p1, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p3, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 1)
+                {
+                    g.DrawLine(p3, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p4, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p1, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 2)
+                {
+                    g.DrawLine(p1, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p3, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p4, X, Y, X + 64, Y);
+                }
+                if (count % 4 == 3)
+                {
+                    g.DrawLine(p, X, Y, X + 32, Y - 64);
+                    g.DrawLine(p, X + 32, Y - 64, X + 64, Y);
+                    g.DrawLine(p, X, Y, X + 64, Y);
+
+                }
+
+            }
+
+
+            p1.Dispose();
+            p4.Dispose();
+            p3.Dispose();
+            p2.Dispose();
+        }
+    }
+}
