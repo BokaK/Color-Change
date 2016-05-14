59eab57f6e5046793c27e3830e77fefac5e2b557
simona069 <markova.simona@yahoo.com>
2016-05-14 21:14:55 +0200

    novi dopolnuvanja vo medium

---
+++ ColorChangeMedium2/WindowsFormsApplication1/Iks.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Iks : Shape
+    {
+        public int count { get; set; }
+        public Iks(float x, float y , int c) : base(x, y)
+        {
+            count = c;
+        }
+        public override void Draw(Graphics g)
+        {
+            Pen p1 = new Pen(Color.Firebrick, 3);
+            Pen p2 = new Pen(Color.IndianRed, 3);
+            Pen p3 = new Pen(Color.RosyBrown, 3);
+            Pen p4 = new Pen(Color.MistyRose, 3);
+            if (count % 4 == 0)
+            {
+                g.DrawLine(p1, X + 20 , Y -30 , X + 50, Y - 30); // -
+                g.DrawLine(p2, X + 50, Y - 30, X + 70, Y - 30);
+                g.DrawLine(p3, X + 50, Y - 30, X + 50, Y - 60);
+                g.DrawLine(p4, X + 50, Y - 30, X + 50, Y);
+           }
+            if (count % 4 == 1)
+            {
+                g.DrawLine(p2, X + 20, Y - 30, X + 50, Y - 30); // -
+                g.DrawLine(p3, X + 50, Y - 30, X + 70, Y - 30);
+                g.DrawLine(p4, X + 50, Y - 30, X + 50, Y - 60);
+                g.DrawLine(p1, X + 50, Y - 30, X + 50, Y);
+            }
+            if (count % 4 == 2)
+            {
+                g.DrawLine(p3, X + 20, Y - 30, X + 50, Y - 30); // -
+                g.DrawLine(p4, X + 50, Y - 30, X + 70, Y - 30);
+                g.DrawLine(p1, X + 50, Y - 30, X + 50, Y - 60);
+                g.DrawLine(p2, X + 50, Y - 30, X + 50, Y);
+            }
+            if (count % 4 == 3)
+            {
+                g.DrawLine(p4, X + 20, Y - 30, X + 50, Y - 30); // -
+                g.DrawLine(p1, X + 50, Y - 30, X + 70, Y - 30);
+                g.DrawLine(p2, X + 50, Y - 30, X + 50, Y - 60);
+                g.DrawLine(p3, X + 50, Y - 30, X + 50, Y);
+            }
+            
+            p1.Dispose();
+            p2.Dispose();
+            p3.Dispose();
+            p4.Dispose();
+        }
+    }
+    
+ }
+
---
+++ ColorChangeMedium2/WindowsFormsApplication1/Scene.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+     public class Scene
+    {
+        public List<Shape> Shapes;
+        public Ball ball { set; get; }
+        public BallsForChange [] balls { get; set; }
+
+        public Scene(int flag)
+        {
+            Shapes = new List<Shape>();
+            ball = new Ball(flag);
+            balls = new BallsForChange[4];
+        }
+
+        public void AddSquare(float x, float y, int c)
+        {
+
+            Shape s = null;
+           
+            s = new Square(x, y, c);
+           
+            Shapes.Add(s);
+
+        }
+        public void AddRotateSquare(float x, float y, int c) {
+            RotateSquarecs s = new RotateSquarecs(x, y, c);
+            Shapes.Add(s);
+        }
+        public void AddIks(float x, float y , int c)
+        {
+            Iks i = new Iks(x, y, c);
+            Shapes.Add(i);
+        }
+        public void AddLines1(float x, float y, int c)
+        {
+
+            Shape s = null;
+
+            s = new Lines1(x, y, c);
+
+            Shapes.Add(s);
+
+        }
+        public void AddVertLinii(float x, float y, int c) {
+            Shape s = new VertLinii(x, y, c);
+            Shapes.Add(s);
+        }
+
+        public void AddCircles(float x, float y, int c)
+        {
+            Shape s = null;
+            s = new TwoCircles(x, y, c);
+            Shapes.Add(s);
+        }
+        public void AddLinii(float x, float y, int c)
+        {
+            Linii l = new Linii(x, y , c);
+            Shapes.Add(l);
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach (Shape s in Shapes)
+            {
+                s.Draw(g);
+            }
+            ball.Draw(g);
+            foreach(BallsForChange b in balls)
+            {
+                b.Draw(g);
+            }
+
+        }
+
+    }
+}
